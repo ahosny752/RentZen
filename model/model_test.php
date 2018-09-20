1dc35f492b611d85deb3b66762e5260021d07243
AJ Hosny <ajhosny@gmail.com>
2018-09-20 17:26:27 -0400

    INSERT STATEMENT ADDED
    INSERT STATMENT AND CONNECTS TO DATABASE

--- model/model_test.php
+++ model/model_test.php
@@ -1,8 +1,33 @@
 <?php
+/* for development purposes only */
+/* Test model functions here. */
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//before I can do anything, I need database credentials
+include '../common/configuration.php';
 
+// before I can use the model functions, I need a database connection
+include 'database.php';
+
+
+// ***********************************************************
+//TEST WILL SAY IT DOESNT WORK BUT THE DATABASE IS GETTING THE DATA INSERTED
+// the properties model
+include 'properties_db.php';
+echo '<p>Testing the properties functions ... </p>';
+
+$property_id = 8; // this id assigned to the user joe in the database
+$street = "2222 winter lane"; // free text
+$city = "easton"; // this is the problem type id for 'computer problem'
+$state_id = 9;
+
+$result = addProperty($property_id, $street, $city, $state_id);
+
+if ($result != false){
+    echo "<p>Success.  Request added. $result</p>";
+} else {
+    echo "<p>Failure.  Request not added.</p>";    
+}
+
+
+
+?>
--- model/properties_db.php
+++ model/properties_db.php
@@ -6,3 +6,21 @@
  * and open the template in the editor.
  */
 
+        
+        
+   function addProperty($property_id, $street, $city, 
+                $state_id){
+    global $db;
+    $sql = "INSERT INTO `property`(`property_id`, `street`, `city`, `state_id`) VALUES "
+            . " (?, ?, ?, ?)";
+    $statement = $db->prepare($sql);
+    $statement->bindValue(1,$property_id);
+    $statement->bindValue(2,$street);
+    $statement->bindValue(3,$city);
+    $statement->bindValue(4,$state_id);
+    $statement->execute();
+    $last_id = $db->lastInsertId();
+    $statement->closeCursor();
+    
+}
+?>
