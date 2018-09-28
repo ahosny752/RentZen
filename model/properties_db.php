5a667fa9229808c9bb0069ff796329419eb1d23e
AJ Hosny <ajhosny@gmail.com>
2018-09-27 23:47:18 -0400

    stuff

--- model/properties_db.php
+++ model/properties_db.php
@@ -11,13 +11,23 @@
    function addProperty($property_id, $street, $city, 
                 $state_id){
     global $db;
-    $sql = "INSERT INTO `property`(`property_id`, `street`, `city`, `state_id`) VALUES "
-            . " (null, ?, ?, ?)";
+    $sql = "INSERT INTO `property`(`property_id`, `street`, `city`, `state_id`,`zip`, `beds`, `baths`, `sqft`, `type_id`, `propstat_id`,`income_requirement`, `credit_requirement`, `rental_fee`, `description`,`picture` VALUES "
+            . " (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, null)";
     $statement = $db->prepare($sql);
     //$statement->bindValue(1,$property_id);
     $statement->bindValue(1,$street);
     $statement->bindValue(2,$city);
     $statement->bindValue(3,$state_id);
+    $statement->bindValue(4,$zip);
+    $statement->bindValue(5,$beds);
+    $statement->bindValue(6,$baths);
+    $statement->bindValue(7,$sqft);
+    $statement->bindValue(8,$type_id);
+    $statement->bindValue(9,$propstat_id);
+    $statement->bindValue(10,$income_requirement);
+    $statement->bindValue(11,$credit_report);
+    $statement->bindValue(12,$rental_fee);
+    $statement->bindValue(13,$description);
     $statement->execute();
     $last_id = $db->lastInsertId();
     $statement->closeCursor();
